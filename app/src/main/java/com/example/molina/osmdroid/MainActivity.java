89f3e3eaf1460dabf2f42950a69553a20545c768
Christian <christian0092@hotmail.com>
2017-10-20 17:40:43 +0200

    Primer commit-mapa

---
+++ app/src/main/java/com/example/molina/osmdroid/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.molina.osmdroid;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.cachemanager.CacheManager;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private static final int REQUEST_CODE = 30;
+    private static final int REQUEST_CODE2 = 21;
+    private GeoPoint centro;
+    private ArrayList<GeoPoint> mi_lista = new ArrayList<GeoPoint>();
+    private CacheManager cacheManager;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Context context= getApplicationContext();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        MapView map = (MapView) findViewById(R.id.map);
+        MapEventsReceiver mReceive = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                Toast.makeText(getBaseContext(), p.getLatitude() + " - " + p.getLongitude(), Toast.LENGTH_LONG).show();
+
+                return false;
+            }
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                return false;
+            }
+        };
+        MapEventsOverlay OverlayEvents = new MapEventsOverlay(getBaseContext(), mReceive);
+        map.getOverlays().add(OverlayEvents);
+        //map.setTileSource(new XYTileSource("osm", 10, 15, 256, ".png", new String[0]));
+        checkLocationPermission();
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE2);
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE);
+        }
+        map.setDrawingCacheEnabled(true);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+
+        centro = new GeoPoint(-35.9869, -62.726641);
+        mi_lista.add(centro);
+        MyLocationNewOverlay localizacion = new MyLocationNewOverlay(new GpsMyLocationProvider(context), map);
+        localizacion.enableMyLocation();
+        mapController.setCenter(centro);
+        cacheManager = new CacheManager(map);
+        CacheManager.CacheManagerCallback aa = new CacheManager.CacheManagerCallback() {
+            @Override
+            public void onTaskComplete() {
+                Toast.makeText(getApplicationContext(), "Descarga finalizada", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void updateProgress(int progress, int currentZoomLevel, int zoomMin, int zoomMax) {
+
+            }
+
+            @Override
+            public void downloadStarted() {
+                Toast.makeText(getApplicationContext(), "empieza a descargarse", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void setPossibleTilesInArea(int total) {
+
+            }
+
+            @Override
+            public void onTaskFailed(int errors) {
+
+            }
+        };
+
+        cacheManager.downloadAreaAsync(this, mi_lista, 0, 15, aa);
+        map.getOverlays().add(localizacion);
+        FolderOverlay poiMarkers = new FolderOverlay(this);
+        map.getOverlays().add(poiMarkers);
+        Marker poiMarker = new Marker(map);
+        poiMarker.setTitle("hola");
+        poiMarker.setSnippet("soy una descripcion");
+        poiMarker.setPosition(centro);
+        poiMarker.setIcon(this.getDrawable(R.drawable.marker_default));
+        poiMarker.setSubDescription(Integer.toString(map.getMaxZoomLevel()));
+        poiMarkers.add(poiMarker);
+
+
+    }
+
+
+    public void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("permiso a la locaclizacion")
+                        .setMessage("Permiso de nose que")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+
+        }
+    }
+}
